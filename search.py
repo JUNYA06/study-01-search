dffa1dce4bc2794513581c05e046afe2566f0ae1
JUNYA06 <junya.mmm.jp.soccer@gmail.com>
2021-08-15 08:00:34 +0900

    0815

--- search.py
+++ search.py
@@ -1,5 +1,16 @@
-### 検索ツールサンプル
-### これをベースに課題の内容を追記してください
+#課題１を作る上で、処理の流れとしては以下のように考えました。
+# リストを生成する処理
+# もしCSVが存在する場合は、読み込む処理
+# 　リストに追加する処理
+
+# 戻り値を受け取る処理
+
+# 戻り値とリストが一致しているか確認する処理
+# 　一致する場合→「見つかった」と出力
+# 　一致しない場合→「見つからなかった」と出力
+
+# csvを出力する処理
+
 import csv
 csv_path = 'C:/Users/user/python/study-01-search/test.csv'
 
@@ -7,48 +18,43 @@ csv_path = 'C:/Users/user/python/study-01-search/test.csv'
 # 検索ソース
 source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
 
-def export_list_csv(export_list,csv_path):
-    with open(csv_path,'w') as f:
-        writer =csv.writer(f,lineterminator='\n')
-
-        if isinstance(export_list[0],list):
-            writer.writerows(export_list)
-        else:
-            writer.writerows(export_list)
-
+def export_list_csv(list,csv_path):
+    with open(csv_path, 'w', newline='', encoding="utf_8") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL,delimiter=';')
+        writer.writerows(list)
 
 def import_list_csv(list,csv_path):
-    with open(csv_path,'r') as f:
-        print(csv_path)
-        reader = csv.reader(f)
+    with open(csv_path,'r', encoding="utf_8") as f:
+        reader = csv.reader(f, lineterminator='\n')
+        
         for row in reader:
-            list.append(row)
+            list.extend(row)
+            
             print(list)
     return list   
 
 ### 検索ツール
 def search():
     import_list =[]
-    import_list_csv(import_list,csv_path)
-    source.extend(import_list)
+    import_list_csv(source,csv_path)
 
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
-### ここに検索ロジックを書く
+
+    #登場人物の検索
     if word in source:
-        print("{}が見つかりした".format(word))
+        print("{}が見つかりました".format(word))
     else:
+        print("{}が見つかりませんでした".format(word))
         source.append(word)
         print(source)
     export_list_csv(source,r"list.csv")
+    
 
 if __name__ == "__main__":
     search()
 
-# もしCSVが存在する場合は、読み込み処理
-# 　リストに追加する処理
 
-# リストを受け取る処理
-# 引数とリストが一致しているか確認する処理
-# 　一致する場合
-# 　一致しない場合
-# csvを出力する処理
+
+
+
+
